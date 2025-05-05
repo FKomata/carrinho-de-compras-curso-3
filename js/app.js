c6880856659e3159c450b2f7ee8c6768ca978256
Felipe Komata <fbkomata@gmail.com>
2025-05-05 12:27:39 -0300

    adição do preço após adicionar itens no carrinho

--- js/app.js
+++ js/app.js
@@ -2,12 +2,22 @@ function adicionar(){
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorProduto = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
-    let resultado = parseInt(quantidade * valorProduto);
+    let quantidade = document.getElementById('quantidade');
+    let resultado = parseInt(quantidade.value * valorProduto);
 
+    let listaDeCompras = document.getElementById('lista-produtos')
+
+    let carrinho = document.getElementById('valor-total').textContent;
+    let valorCarrinho = parseInt(carrinho.split('R$')[1]);
+    let valorTotal = valorCarrinho + resultado;
+    document.getElementById('valor-total').innerHTML = valorTotal
     
+
     console.log(valorProduto);
     console.log(nomeProduto);
-    console.log(quantidade);
+    console.log(quantidade.value);
     console.log(resultado);
+    console.log(listaDeCompras)
+    console.log(valorTotal)
+
 }
