a8c825eef6511bacce2d1f196ba29b3b83615407
Felipe Komata <fbkomata@gmail.com>
2025-05-05 17:28:44 -0300

    correção do resultado final e adição ne produtos no carrinho

--- js/app.js
+++ js/app.js
@@ -2,22 +2,23 @@ function adicionar(){
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorProduto = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    let resultado = parseInt(quantidade.value * valorProduto);
+    let quantidade = document.getElementById('quantidade').value;
+    let resultado = parseInt(quantidade * valorProduto);
 
-    let listaDeCompras = document.getElementById('lista-produtos')
-
-    let carrinho = document.getElementById('valor-total').textContent;
-    let valorCarrinho = parseInt(carrinho.split('R$')[1]);
-    let valorTotal = valorCarrinho + resultado;
-    document.getElementById('valor-total').innerHTML = valorTotal
+    let listaDeCompras = document.getElementById('lista-produtos');
+    listaDeCompras.innerHTML = listaDeCompras.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>
+        </section>`;
     
+    //let carrinho = document.getElementById('valor-total').textContent;
+//let valorCarrinho = parseInt(carrinho.split('R$')[1]);
+    //let valorTotal = valorCarrinho + resultado;
+    //document.getElementById('valor-total').innerHTML = valorTotal
 
-    console.log(valorProduto);
-    console.log(nomeProduto);
-    console.log(quantidade.value);
-    console.log(resultado);
-    console.log(listaDeCompras)
-    console.log(valorTotal)
+    //let carrinho = document.getElementById('valor-total');
+   // carrinho.innerHTML = carrinho.innerHTML +  `<span class="texto-azul" id="valor-total">${resultado}</span>`;
 
+    //console.log(carrinho);
+    
+    
 }
